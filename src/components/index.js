57a8856b6f8d646907337ab505800eb61bdb330d
Aravind K <araviku04@gmail.com>
2025-11-07 14:38:49 -0600

    Fix casing of CountryPicker import

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,3 @@
 export { default as Chart } from './chart/Chart';
-export { default as CountryPicker } from './countrypicker/CountryPicker';
+export { default as CountryPicker } from './countrypicker/Countrypicker';
 export { default as Cards } from './cards/Cards';
